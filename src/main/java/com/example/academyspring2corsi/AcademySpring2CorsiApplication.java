19f0882e137ec57be2656e888bf3165f7036f74a
L3me05 <l.dimarcantonio@elitesoftwarehouse.com>
2025-05-28 11:39:40 +0200

    primo commit

---
+++ src/main/java/com/example/academyspring2corsi/AcademySpring2CorsiApplication.java
@@ -0,0 +1,13 @@
+package com.example.academyspring2corsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademySpring2CorsiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademySpring2CorsiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/academyspring2corsi/AcademySpring2CorsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.academyspring2corsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademySpring2CorsiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
